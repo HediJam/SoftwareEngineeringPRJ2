8be4fcdb1dbeb1a1fdf63175d4af1d9e6d234d2e
HediJam <hedieh_jam@live.com>
2015-11-05 12:41:40 +0330

    server and client work with hello message and processing strings

---
+++ src/main/java/client/Terminal.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author SARIR
+ */
+public class Terminal {
+
+    private BufferedReader in;
+    private PrintWriter out;
+    private JFrame frame = new JFrame("Capitalize Client");
+    private JTextField dataField = new JTextField(40);
+    private JTextArea messageArea = new JTextArea(8, 60);
+
+    /**
+     * Constructs the client by laying out the GUI and registering a listener
+     * with the textfield so that pressing Enter in the listener sends the
+     * textfield contents to the server.
+     */
+    public Terminal() {
+
+        // Layout GUI
+        messageArea.setEditable(false);
+        frame.getContentPane().add(dataField, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+
+        // Add Listeners
+        dataField.addActionListener(new ActionListener() {
+            /**
+             * Responds to pressing the enter key in the textfield by sending
+             * the contents of the text field to the server and displaying the
+             * response from the server in the text area. If the response is "."
+             * we exit the whole application, which closes all sockets, streams
+             * and windows.
+             */
+            public void actionPerformed(ActionEvent e) {
+                out.println(dataField.getText());
+                String response;
+                try {
+                    response = in.readLine();
+                    if (response == null || response.equals("")) {
+                        System.exit(0);
+                    }
+                } catch (IOException ex) {
+                    response = "Error: " + ex;
+                }
+                messageArea.append(response + "\n");
+                dataField.selectAll();
+            }
+        });
+    }
+
+    /**
+     * Implements the connection logic by prompting the end user for the
+     * server's IP address, connecting, setting up streams, and consuming the
+     * welcome messages from the server. The Capitalizer protocol says that the
+     * server sends three lines of text to the client immediately after
+     * establishing a connection.
+     */
+    public void connectToServer() throws IOException {
+
+        // Get the server address from a dialog box.
+        String serverAddress = JOptionPane.showInputDialog(
+                frame,
+                "Enter IP Address of the Server:",
+                "Welcome to the Capitalization Program",
+                JOptionPane.QUESTION_MESSAGE);
+
+        // Make connection and initialize streams
+        Socket socket = new Socket(serverAddress, 9898);
+        in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Consume the initial welcoming messages from the server
+        for (int i = 0; i < 3; i++) {
+            messageArea.append(in.readLine() + "\n");
+        }
+    }
+
+    /**
+     * Runs the client application.
+     */
+    public static void main(String[] args) throws Exception {
+
+        String sentence;
+        String modifiedSentence = null;
+        BufferedReader inFromUser = null;
+        Socket clientSocket = new Socket("localhost", 9898);
+
+        inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.println("aval while");
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+         modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+        
+        while (true) {
+            System.out.println("d");
+            sentence = inFromUser.readLine();
+            outToServer.writeBytes(sentence + '\n');
+            modifiedSentence = inFromServer.readLine(); 
+            System.out.println("From server :" + modifiedSentence) ;
+        }
+    }
+
+}
